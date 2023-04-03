7da64ad33f2e835045a19f2bf602a62d3dcd2088
Michael <humphries_michael@hotmail.com>
2023-04-03 16:19:30 +0100

    removed .map v3

--- src/searchMap/components/searchMapNav.js
+++ src/searchMap/components/searchMapNav.js
@@ -165,7 +165,7 @@ const searchbarRef = useRef(null);
 
 
 {/* <div class={props.largeNav === "location" ? "_xhcgu32" : "_yu5dnq"}> */}
-                { activeSearch ?    <div class="_xhcgu32">
+                {/* activeSearch ?    <div class="_xhcgu32">
                       <div
                         class="c1nifi44 l16t0m55 dir dir-ltr"
                         data-testid="structured-search-input-field-query-panel"
@@ -200,7 +200,7 @@ const searchbarRef = useRef(null);
                           </div>
                         </section>
                       </div>
-                    </div> : null }
+                    </div> : null */}
 
                   </div>
                 </div>
