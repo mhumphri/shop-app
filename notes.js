1e2d138322f2a6d4b63844109f1776882bad9548
Michael <humphries_michael@hotmail.com>
2023-04-24 23:45:18 +0100

    after failed branch merge

--- notes.js
+++ notes.js
@@ -5,3 +5,6 @@ chrome links not working
 https://stackoverflow.com/questions/20012821/buttons-not-working-in-chrome-app
 
 git push origin swipable-gallery
+
+// get merge branch into main
+https://stackoverflow.com/questions/33503686/how-do-i-merge-a-branch-into-a-master-in-github
