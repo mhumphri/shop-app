08ec2fdfa8b3d9ccabf5432427550ff606acf797
Michael <humphries_michael@hotmail.com>
2023-09-01 23:58:25 +0100

    added footer

--- src/searchPage/components/searchPage.js
+++ src/searchPage/components/searchPage.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../header/components/header";
+import Footer from "../../footer/components/footer";
 
 
 function SearchPage() {
 
   return (
+    <>
     <Header largeView={true} noBasket={true} />
+    <Footer />
+    </>
   )
 
 }
