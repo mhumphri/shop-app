458b92a95640d1e3d99eab74b905d6725446fefa
Michael <humphries_michael@hotmail.com>
2023-09-02 21:23:45 +0100

    added partial html for productSearch

--- src/searchPage/components/productSearch.js
+++ src/searchPage/components/productSearch.js
@@ -1,15 +1,152 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { categoryArray, itemsArray } from "./data";
 import Header from "../../header/components/header";
 
 
 function ProductSearch() {
 
+  // viewport width (stored in redux)
+  const screenWidth = useSelector((state) => state.deviceData.screenWidth);
+
+  // stors width and height of category menu button - used to style category dropdown
+  const [categoryMenuWidth, setCategoryMenuWidth] = useState();
+  const [categoryMenuHeight, setCategoryMenuHeight] = useState();
+
+  // ref for category menu button - dimensions used to style category dropdown
+  const categoryMenuRef = useRef();
+  // ref for category menu dropdown - used for closing it when clicks are detected outside
+  const categoryDropdownRef = useRef();
+
+  // boolean which controls category menu state (i.e. open or closed)
+  const [categoryMenuOpen, setCategoryMenuOpen] = useState();
+  // currently selected item category - used for formatting category menu dropdown and controlling displayed items
+  const [activeCategory, setActiveCategory] = useState({
+    name: "All",
+    numberItems: 27,
+  });
+  const [activeItemsArray, setActiveItemsArray] = useState(itemsArray);
+
+  const openMenu = () => {
+    setCategoryMenuOpen(true);
+  };
+
+  const selectItemCategory = (newCategory) => {
+    setActiveCategory(newCategory);
+    setCategoryMenuOpen(false);
+
+    let newItemsArray = [];
+    if (newCategory.name === "All") {
+      newItemsArray = itemsArray;
+    } else {
+      for (let i = 0; i < itemsArray.length; i++) {
+        if (itemsArray[i].category === newCategory.name) {
+          newItemsArray.push(itemsArray[i]);
+        }
+      }
+    }
+    setActiveItemsArray(newItemsArray);
+  };
+
   return (
-    <>
-    ProductSearch
-    </>
-  )
+    <section>
+      <div className="product-search-xd3">
+        <div className="product-search-sm4">
+          <button
+            className={
+              categoryMenuOpen
+                ? "product-search-ke7 menu-active"
+                : "product-search-ke7"
+            }
+            ref={categoryMenuRef}
+            onClick={openMenu}
+          >
+            <div className="product-search-ll2">
+              {activeCategory.name} ({activeCategory.numberItems})
+            </div>
+            <span className="product-search-fr8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <polygon points="16.5 10 12 16 7.5 10 16.5 10"></polygon>
+              </svg>
+            </span>
+          </button>
+          <div
+            ref={categoryDropdownRef}
+            className={
+              categoryMenuOpen
+                ? "product-search-vs5 menu-open"
+                : "product-search-vs5"
+            }
+            style={{
+              minWidth: categoryMenuWidth + "px",
+              paddingTop: categoryMenuHeight + "px",
+            }}
+          >
+            {categoryArray.map((x) => (
+              <button
+                key={x.name}
+                className={
+                  activeCategory.name === x.name
+                    ? "product-search-lx7 selected"
+                    : "product-search-lx7"
+                }
+                onClick={() => selectItemCategory(x)}
+              >
+                {x.name} ({x.numberItems})
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="product-search-hd5">
+        {/*  <BasketButton /> */}
+        </div>
+      </div>
+      <div className="product-search-cv2">
+        <div className="product-search-ty1">
+          {activeItemsArray.map((x, index) => (
+            <div key={x.name} className="product-search-wq3">
+              <Link
+                to={"/product/" + index}
+                className="product-search-us2"
+              >
+                <div className="product-search-uu4">
+                  <div className="product-search-dj9">
+                    <div className="product-search-zq1">
+                      <div className="product-search-ry4">
+                        <img
+                          className="product-search-ks8"
+                          src={x.imageUrl}
+                          /*srcset="https://i.etsystatic.com/34623276/c/2442/1941/250/125/il/400df9/4712972989/il_340x270.4712972989_krpc.jpg 340w,https://i.etsystatic.com/34623276/c/2442/1941/250/125/il/400df9/4712972989/il_680x540.4712972989_krpc.jpg 680w"
+                      sizes="(max-width: 9223372036854775807px) 15vw, (max-width: 1679px) 20vw, (max-width: 1199px) 25vw, (max-width: 899px) 33vw, 50vw"*/
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="product-search-fg6">
+                  <h3 className="product-search-nh2">{x.name}</h3>
+                  <div className="product-search-iu4">£{x.price}</div>
+                </div>
+              </Link>
+            </div>
+          ))}
+          <div className="product-search-ke9">
+
+            <p className="product-search-jw5">Can't find what you're looking for?</p>
+            <div className="product-search-uw7">
+            {/*<ContactButton /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 
 }
 
